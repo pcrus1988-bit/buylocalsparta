091f31fa3c1d25981253116b895a6d9080e862ab
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-17 15:30:22 +0300

    Load merchant discovery styles

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./storefront-merchants.css";
 import { CartProvider } from "../components/CartProvider";
 
 export const metadata: Metadata = {
