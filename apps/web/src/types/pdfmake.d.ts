e5d315a7ff595defcdbe461848c31e38d7586904
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-19 11:03:33 +0300

    Add pdfmake build module declarations

---
+++ apps/web/src/types/pdfmake.d.ts
@@ -0,0 +1,9 @@
+declare module "pdfmake/build/pdfmake.js" {
+  const pdfMake: any;
+  export default pdfMake;
+}
+
+declare module "pdfmake/build/vfs_fonts.js" {
+  const vfsFonts: any;
+  export default vfsFonts;
+}
