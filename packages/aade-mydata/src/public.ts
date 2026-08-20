4afaf4893a094514c46b17bc73d749afcc2566f9
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-20 15:02:52 +0300

    Export schema order preflight API

--- packages/aade-mydata/src/public.ts
+++ packages/aade-mydata/src/public.ts
@@ -3,4 +3,5 @@ export * from "./catalog.ts";
 export * from "./preflight.ts";
 export * from "./classification-preflight.ts";
 export * from "./payment-method-preflight.ts";
+export * from "./order-preflight.ts";
 export { HardenedAadeMyDataClient as AadeMyDataClient, type MyDataReportingQuery } from "./hardened-client.ts";
