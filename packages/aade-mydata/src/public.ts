b893e4783b9cc7aead41837e4f694835226e0f13
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-20 14:54:41 +0300

    Export classification preflight API

--- packages/aade-mydata/src/public.ts
+++ packages/aade-mydata/src/public.ts
@@ -1,4 +1,5 @@
 export * from "./index.ts";
 export * from "./catalog.ts";
 export * from "./preflight.ts";
+export * from "./classification-preflight.ts";
 export { HardenedAadeMyDataClient as AadeMyDataClient } from "./hardened-client.ts";
