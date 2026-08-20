272b99a7b63bfdb1ff1ca775907392f73b82bd92
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-20 14:54:29 +0300

    Enforce classification preflight before AADE send

--- packages/aade-mydata/src/hardened-client.ts
+++ packages/aade-mydata/src/hardened-client.ts
@@ -1,9 +1,11 @@
 import { AadeMyDataClient as BaseAadeMyDataClient, type MyDataTransmissionResult } from "./index.ts";
+import { assertClassificationXmlPreflight } from "./classification-preflight.ts";
 import { assertInvoiceXmlPreflight } from "./preflight.ts";
 
 export class HardenedAadeMyDataClient extends BaseAadeMyDataClient {
   override async sendInvoices(xml: string): Promise<MyDataTransmissionResult> {
     assertInvoiceXmlPreflight(xml);
+    assertClassificationXmlPreflight(xml);
     return super.sendInvoices(xml);
   }
 }
