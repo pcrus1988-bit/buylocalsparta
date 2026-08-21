fd10a124b91537f9c83828b8aacf7de2c2eb6697
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-21 22:07:00 +0300

    Load saved-search editing styles

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -22,6 +22,7 @@ import "./customer-support.css";
 import "./customer-returns-lifecycle.css";
 import "./customer-fulfilment-progress.css";
 import "./customer-notification-lifecycle.css";
+import "./customer-saved-search-editing.css";
 import "./vendor-information-architecture.css";
 import "./vendor-lifecycle.css";
 import "./vendor-login.css";
