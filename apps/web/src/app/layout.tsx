04db8bead027fbaa8b729dd5a9c1645b38d87b6c
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-21 18:55:09 +0300

    Load customer return lifecycle styles

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -18,6 +18,7 @@ import "./customer-account-ux.css";
 import "./ask-local-clarifications.css";
 import "./customer-profile-security.css";
 import "./customer-support.css";
+import "./customer-returns-lifecycle.css";
 import "./vendor-information-architecture.css";
 import "./vendor-lifecycle.css";
 import "./vendor-login.css";
