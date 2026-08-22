3124c79c926a66859249e63ad1a5591561be3eee
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-23 01:21:53 +0300

    Narrow proxy dependency to CMS redirect resolver

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { getActivePublicCmsRedirect } from "./lib/public-cms";
+import { getActivePublicCmsRedirect } from "./lib/public-cms-redirects";
 
 const MARKETPLACE_COOKIE = "bls_marketplace";
 const LEGACY_VISITOR_COOKIE = "bls_visitor";
