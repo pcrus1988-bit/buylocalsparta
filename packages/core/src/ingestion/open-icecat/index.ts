97d9049a62b1b0adbe15410a85a02d65d74ecfee
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-29 14:58:51 +0300

    feat(catalog): export Icecat streaming runner

--- packages/core/src/ingestion/open-icecat/index.ts
+++ packages/core/src/ingestion/open-icecat/index.ts
@@ -1,6 +1,8 @@
 export * from "./types.ts";
 export { isValidGtin, sanitizeIcecatPayload, stripContentToken } from "./utils.ts";
 export * from "./index-csv.ts";
+export * from "./index-stream.ts";
+export * from "./bulk-runner.ts";
 export * from "./localization.ts";
 export * from "./product.ts";
 export * from "./client.ts";
