76c6528801b8fb211bb125bbe59eed7259a32518
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-29 12:06:23 +0300

    fix(catalog): avoid ingestion normalizeGtin export collision

--- packages/core/src/ingestion/open-icecat/index.ts
+++ packages/core/src/ingestion/open-icecat/index.ts
@@ -1,5 +1,5 @@
 export * from "./types.ts";
-export * from "./utils.ts";
+export { isValidGtin, sanitizeIcecatPayload, stripContentToken } from "./utils.ts";
 export * from "./index-csv.ts";
 export * from "./localization.ts";
 export * from "./product.ts";
