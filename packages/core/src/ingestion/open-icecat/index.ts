92d12b93646886e7868ddacd1266caf752cc45f9
pcrus1988-bit <pcrus1988@gmail.com>
2026-08-29 12:00:48 +0300

    feat(catalog): expose Open Icecat ingestion facade

---
+++ packages/core/src/ingestion/open-icecat/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.ts";
+export * from "./utils.ts";
+export * from "./index-csv.ts";
+export * from "./localization.ts";
+export * from "./product.ts";
+export * from "./client.ts";
