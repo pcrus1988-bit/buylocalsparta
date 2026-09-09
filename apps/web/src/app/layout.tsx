c09781aa6bde39a3f0e6161f2b8168a4f58ce8ea
pcrus1988-bit <pcrus1988@gmail.com>
2026-09-10 00:12:55 +0300

    feat(typography): activate premium storefront font layer

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -53,6 +53,7 @@ import "./site-utility-launcher.css";
 import "./customer-mobile-commerce.css";
 import "./public-release-readiness.css";
 import "./premium-marketplace.css";
+import "./premium-typography.css";
 import { CartProvider } from "../components/CartProvider";
 import { CustomerMobileCommerceProvider } from "../components/CustomerMobileCommerceNav";
 import { PrivacyConsentProvider } from "../components/PrivacyConsentProvider";
