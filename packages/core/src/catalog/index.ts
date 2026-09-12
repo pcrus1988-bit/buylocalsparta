edaad2186475a478bea4c03cc696c2f894720b32
pcrus1988-bit <pcrus1988@gmail.com>
2026-09-12 11:07:12 +0300

    refactor(catalogue): restore direct price-free management export

--- packages/core/src/catalog/index.ts
+++ packages/core/src/catalog/index.ts
@@ -1,15 +1,6 @@
 export * from "./types.ts";
 export * from "./matching.ts";
-export {
-  type VendorProductSubmissionStatus,
-  type MatchCandidateStatus,
-  type CatalogFulfilmentMode,
-  type CanonicalCatalogProduct,
-  type VendorProductSubmission,
-  type ProductMatchCandidate,
-  type CatalogWorkflowEvent
-} from "./management.ts";
-export { CatalogManagementService } from "./management-compat.ts";
+export * from "./management.ts";
 export * from "./import.ts";
 export * from "./governance.ts";
 export * from "./colors.ts";
